1968cba03b1dfe887acd044d150ee6f1598ff053
Felix Jones <felix@felixjones.co.uk>
2020-02-16 21:48:11 +0100

    Initial commit

---
+++ source/main.iwram.cpp
@@ -0,0 +1,17 @@
+﻿#define EVER ;;
+
+int main(int argc, char* argv[]) {
+	*(unsigned int*)0x04000000 = 0x0403;
+
+	int t = 0;
+	for (EVER) {
+		int x, y;
+		for (x = 0; x < 240; ++x) {
+			for (y = 0; y < 160; ++y) {
+				((unsigned short*)0x06000000)[x + y * 240] = ((((x & y) + t) & 0x1F) << 10) | ((((x & y) + t * 3) & 0x1F) << 5) | ((((x & y) + t * 5) & 0x1F) << 0);
+			}
+		}
+		++t;
+	}
+	return 0;
+}
