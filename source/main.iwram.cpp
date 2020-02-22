221c746cd506aea8d8e86c29579c740a59eb2b5b
Felix Jones <felix@felixjones.co.uk>
2020-02-22 22:03:00 +0100

    Set Clang headers

--- source/main.iwram.cpp
+++ source/main.iwram.cpp
@@ -1,6 +1,9 @@
-﻿#define EVER ;;
+﻿#include <cstdlib>
+
+#define EVER ;;
 
 int main(int argc, char* argv[]) {
+	auto data = (volatile unsigned short*)std::malloc(sizeof(short));
 	*(volatile unsigned int*)0x04000000 = 0x0403;
 
 	int t = 0;
@@ -8,7 +11,8 @@ int main(int argc, char* argv[]) {
 		int x, y;
 		for (x = 0; x < 240; ++x) {
 			for (y = 0; y < 160; ++y) {
-				((volatile unsigned short*)0x06000000)[x + y * 240] = ((((x & y) + t) & 0x1F) << 10) | ((((x & y) + t * 3) & 0x1F) << 5) | ((((x & y) + t * 5) & 0x1F) << 0);
+				*data = ((((x & y) + t) & 0x1F) << 10) | ((((x & y) + t * 3) & 0x1F) << 5) | ((((x & y) + t * 5) & 0x1F) << 0);
+				((volatile unsigned short*)0x06000000)[x + y * 240] = *data;
 			}
 		}
 		++t;
