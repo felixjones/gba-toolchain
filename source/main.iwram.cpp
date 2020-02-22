eafca9b40bdb80816c3d5f1ae178cd4b9b22b6d2
Felix Jones <felix@felixjones.co.uk>
2020-02-22 11:56:50 +0100

    Added ROM fixing and Multiboot configuration.

--- source/main.iwram.cpp
+++ source/main.iwram.cpp
@@ -1,14 +1,14 @@
 ﻿#define EVER ;;
 
 int main(int argc, char* argv[]) {
-	*(unsigned int*)0x04000000 = 0x0403;
+	*(volatile unsigned int*)0x04000000 = 0x0403;
 
 	int t = 0;
 	for (EVER) {
 		int x, y;
 		for (x = 0; x < 240; ++x) {
 			for (y = 0; y < 160; ++y) {
-				((unsigned short*)0x06000000)[x + y * 240] = ((((x & y) + t) & 0x1F) << 10) | ((((x & y) + t * 3) & 0x1F) << 5) | ((((x & y) + t * 5) & 0x1F) << 0);
+				((volatile unsigned short*)0x06000000)[x + y * 240] = ((((x & y) + t) & 0x1F) << 10) | ((((x & y) + t * 3) & 0x1F) << 5) | ((((x & y) + t * 5) & 0x1F) << 0);
 			}
 		}
 		++t;
