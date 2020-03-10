e463128f2385efc5d481dccb5bf5eec830b6f4d8
Felix Jones <felix@felixjones.co.uk>
2020-03-10 19:30:10 +0100

    Added support for gbaplusplus jump_table IRQ

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,26 @@
 
 using namespace gba;
 
+[[gnu::target( "arm" ), gnu::section( ".iwram" )]]
+static void vblank_handler() {
+	static int t = 0;
+
+	for ( int x = 0; x < 240; x++ ) {
+		for ( int y = 0; y < 160; y++ ) {
+			( ( volatile unsigned short * )0x06000000)[x + y * 240] = 
+				( ( ( ( x & y ) + t ) & 0x1F ) << 10 ) | 
+				( ( ( ( x & y ) + t * 3 ) & 0x1F ) << 5 ) |
+				( ( ( ( x & y ) + t * 5 ) & 0x1F ) << 0 );
+		}
+	}
+	t++;
+}
+
+[[gnu::section( ".iwram" )]]
+static constexpr auto irq_handler = jump_table( interrupt_condition { 0x1, vblank_handler } );
+
 int main(int argc, char* argv[]) {
-	io::interrupt_handler::write( interrupt_handler::empty_handler );
+	interrupt_handler::set( irq_handler );
 
 	io::display_control::write( display_control::make( []( display_control& v ) {
 		v.mode = 3;
@@ -22,16 +40,7 @@ int main(int argc, char* argv[]) {
 
 	io::interrupt_master_enable::write( true );
 
-	int t = 0;
-	for (EVER) {
-		int x, y;
-		for (x = 0; x < 240; ++x) {
-			for (y = 0; y < 160; ++y) {
-				((volatile unsigned short*)0x06000000)[x + y * 240] = ((((x & y) + t) & 0x1F) << 10) | ((((x & y) + t * 3) & 0x1F) << 5) | ((((x & y) + t * 5) & 0x1F) << 0);
-			}
-		}
-		++t;
-
+	for ( EVER ) {
 		bios::vblank_intr_wait();
 	}
 
