4ed08f827ace58df0bc5e2edf7741c1e83c6de34
Felix Jones <felix@felixjones.co.uk>
2022-02-28 00:44:26 +0100

    Re-written librom crt0.s ROM header moved into include file Separated _sbrk into gba-sbrk.c Cleaned up gba-nosyscalls.c atexit and lock data and bss uses EWRAM

---
+++ lib/gba-nosyscalls.c
@@ -0,0 +1,58 @@
+/*
+===============================================================================
+
+ Empty syscalls
+
+ Copyright (C) 2021-2022 gba-toolchain contributors
+ For conditions of distribution and use, see copyright notice in LICENSE.md
+
+===============================================================================
+*/
+
+#include <sys/stat.h>
+#include <sys/times.h>
+#include <sys/unistd.h>
+#include <errno.h>
+
+#undef errno
+extern int errno;
+#if defined(__DEVKITARM__)
+int errno; // devkitARM lacks errno
+#endif
+
+pid_t _getpid() {
+    return 0;
+}
+
+static int _gba_nosys() {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _close(int fd) __attribute__((alias("_gba_nosys")));
+
+int _execve(const char* __restrict__ pathname, char* const __restrict__ argv[], char* const __restrict__ envp[]) __attribute__((alias("_gba_nosys")));
+
+pid_t _fork() __attribute__((alias("_gba_nosys")));
+
+int _fstat(int fd, struct stat* statbuf) __attribute__((alias("_gba_nosys")));
+
+int _kill(pid_t pid, int sig) __attribute__((alias("_gba_nosys")));
+
+int _link(const char* __restrict__ oldpath, const char* __restrict__ newpath) __attribute__((alias("_gba_nosys")));
+
+int _lseek(int fd, off_t offset, int whence) __attribute__((alias("_gba_nosys")));
+
+int _open(const char* __restrict__ pathname, int flags, mode_t mode) __attribute__((alias("_gba_nosys")));
+
+int _read(int fd, void* buf, size_t count) __attribute__((alias("_gba_nosys")));
+
+int _stat(const char* __restrict__ pathname, struct stat* __restrict__ statbuf) __attribute__((alias("_gba_nosys")));
+
+int _times(struct tms* buf) __attribute__((alias("_gba_nosys")));
+
+int _unlink(const char* pathname) __attribute__((alias("_gba_nosys")));
+
+pid_t _wait(int* wstatus) __attribute__((alias("_gba_nosys")));
+
+ssize_t _write(int fd, const void* buf, size_t count) __attribute__((alias("_gba_nosys")));
---
+++ lib/gba-sbrk.c
@@ -0,0 +1,39 @@
+/*
+===============================================================================
+
+ _sbrk
+
+ Copyright (C) 2021-2022 gba-toolchain contributors
+ For conditions of distribution and use, see copyright notice in LICENSE.md
+
+===============================================================================
+*/
+
+#include <errno.h>
+#include <stdint.h>
+
+#undef errno
+extern int errno;
+
+#define EWRAM_TOP 0x2040000
+
+char* _sbrk(int incr) {
+    extern char __ewram_end;
+    static char * heap_end = &__ewram_end;
+
+    if ((uintptr_t) (heap_end + incr) >= EWRAM_TOP) {
+        errno = ENOMEM;
+        return (char*) -1;
+    }
+
+    char* prev_heap_end = heap_end;
+    heap_end += incr;
+    return prev_heap_end;
+}
+
+#if defined(__DEVKITARM__)
+// devkitARM uses _sbrk_r
+char* _sbrk_r(__attribute__((unused)) void* reent, int incr) {
+    return _sbrk(incr);
+}
+#endif
