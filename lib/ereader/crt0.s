447fbe259b7d00916d639b8c43e1758498971275
Felix Jones <felix@felixjones.co.uk>
2022-03-01 13:27:41 +0100

    Re-written libereader crt0.s

--- lib/ereader/crt0.s
+++ lib/ereader/crt0.s
@@ -9,22 +9,27 @@
 ===============================================================================
 */
 
-    .section .crt0, "ax"
     .align 2
+    .section .crt0, "ax"
+
+    // start
     .arm
-    .global _start
-_start:
+    .global __start
+__start:
     // Jump to ARM entry point
-    b       .Larm_start
+    b       _start
+    .global __dso_handle // Dynamic Shared Object Handle
+__dso_handle:
 .Lzero_word:
     .word   0x00000000
 
     // This word is destroyed by e-reader
     .word   0x00000000
 
-    // EWRAM (arm) execution entry point
+    // start
     .arm
-.Larm_start:
+    .global _start
+_start:
     // Switch to IRQ mode (0x12)
     mov     r0, #0x12
     msr     cpsr, r0
@@ -62,50 +67,106 @@ _start:
     ldm     r0, {r0-r2}
     swi     #0xb
 
-    // Initializers
-    .extern __libc_init_array
-    ldr     r2, =__libc_init_array
-    bl      .Lbx_r2
+    // preinit_array (r0-r3 are clobbered, r4 is used for .Lbx_r4, so use r5-r6)
+    ldr     r5, =__preinit_array_start
+    ldr     r6, =__preinit_array_end
+    bl      .Lcall_array
 
-    // Main
+    // init_array (r0-r3 are clobbered, r4 is used for .Lbx_r4, so use r5-r6)
+    ldr     r5, =__init_array_start
+    ldr     r6, =__init_array_end
+    bl      .Lcall_array
+
+    // main
     mov     r0, #0 // argc
-    mov     r1, #0 // argv
-    .extern main
-    ldr     r2, =main
-    bl      .Lbx_r2
+    mov     r1, #0 // argv (NULL)
+    ldr     r4, =main
+    bl      .Lbx_r4
 
-    // Finalizers
-    .extern __libc_fini_array
-    ldr     r2, =__libc_fini_array
-    push    {r0}
-    bl      .Lbx_r2
-    pop     {r0}
+#ifndef __NO_FINI__
+    push    {r0, r1} // Push exit code (r1 for alignment)
 
+    // Disable REG_IME by setting lowest bit to zero (using lowest bit of REG_IME)
+    ldr     r1, =#0x4000208
+    str     r1, [r1]
+
+    // fini_array
+    ldr     r5, =__fini_array_start
+    ldr     r6, =__fini_array_end
+    bl      .Lrcall_array
+
+    pop     {r0, r1} // Pop exit code (r1 for alignment)
+#endif
+
+    b       exit
+
+    .thumb_func
+.Lpop_r3_bx_r4:
+    pop     {r3-r4}
+    // Fallthrough to .Lbx_r4
+
+    .thumb_func
+.Lbx_r4:
+    bx      r4
+
+    // call_array (r4 is used for .Lpop_r3_bx_r4, r5-r6 is used for array)
+    .thumb_func
+.Lcall_array:
+    push    {r3, lr} // r3 pushed for alignment
+.Lcall_array_loop:
+    cmp     r5, r6
+    beq     .Lpop_r3_bx_r4
+    ldr     r4, [r5]
+    add     r5, #4
+    bl      .Lbx_r4
+    b       .Lcall_array_loop
+
+#ifndef __NO_FINI__
+    // rcall_array (r4 is used for .Lpop_r3_bx_r4, r5-r6 is used for array)
+    .thumb_func
+.Lrcall_array:
+    push    {r3, lr} // r3 pushed for alignment
+.Lrcall_array_loop:
+    cmp     r5, r6
+    beq     .Lpop_r3_bx_r4
+    sub     r6, #4
+    ldr     r4, [r6]
+    bl      .Lbx_r4
+    b       .Lrcall_array_loop
+#endif
+
+    // exit
+    .thumb_func
+    .global exit
+exit:
+#ifndef __NO_FINI__
+    push    {r0-r1} // Push exit code (r1 for alignment)
+
+    // Disable REG_IME by setting lowest bit to zero (using lowest bit of REG_IME)
+    ldr     r1, =#0x4000208
+    str     r1, [r1]
+
+    mov     r1, #0 // NULL
+    ldr     r4, =__call_exitprocs
+    bl      .Lbx_r4
+    pop     {r0, r1} // Pop exit code (r1 for alignment)
+#endif
     // Fallthrough to _exit
+
     .thumb_func
     .global _exit
 _exit:
+    // Disable REG_IME by setting lowest bit to zero (using lowest bit of REG_IME)
+    ldr     r0, =#0x4000208
+    str     r0, [r0]
+
     swi     #0x00 // Soft reset
 
-    // For branch-link
-    .thumb
-.Lbx_r2:
-    bx      r2
-
-    // Reference symbols from gba-syscalls.c to prevent removal
+    // Reference _sbrk, _getpid to prevent removal
     .global _sbrk
-    .global _kill
-
-    // Placed in own section to allow gc-section removal
-    .section .crt0._getpid, "ax", %progbits
-    .thumb_func
     .global _getpid
-_getpid:
-    mov     r0, #0
-    // Fallthrough to __sync_synchronize
 
-    .section .crt0.__sync_synchronize, "ax", %progbits
-    .thumb_func
-    .global __sync_synchronize
-__sync_synchronize:
-    bx      lr
+#ifndef __NO_FINI__
+    // Reference __register_exitproc to enable static destructors atexit
+    .global __register_exitproc
+#endif
