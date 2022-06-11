eec34fc225d29651818222e3c9f21bebb934b094
Felix Jones <felix@felixjones.co.uk>
2022-06-11 23:37:12 +0200

    Updated libseven sample

--- samples/02 libseven/main.c
+++ samples/02 libseven/main.c
@@ -1,20 +1,13 @@
-#include <seven/prelude.h>
 #include <seven/video/prelude.h>
 #include <seven/video/bg_bitmap.h>
 
 int main(void)
 {
-    irqInitDefault();
-    irqEnableFull(IRQ_VBLANK);
-
     REG_DISPCNT = VIDEO_MODE_BITMAP | VIDEO_BG2_ENABLE;
 
     MODE3_FRAME[80][120] = COLOR_RED;
     MODE3_FRAME[80][136] = COLOR_GREEN;
     MODE3_FRAME[96][120] = COLOR_BLUE;
 
-    while (1)
-    {
-        svcVBlankIntrWait();
-    }
+    while (1) {}
 }
