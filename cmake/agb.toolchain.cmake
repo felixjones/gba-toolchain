4e3f291d2f75b9eeace4d0520708d058f3b706fd
Felix Jones <felix.jones@mojang.com>
2022-12-16 17:14:06 +0100

    Trying to find sysroot

--- cmake/agb.toolchain.cmake
+++ cmake/agb.toolchain.cmake
@@ -62,3 +62,25 @@ endfunction()
 find_arm_compiler(ASM arm-none-eabi-as)
 find_arm_compiler(C arm-none-eabi-gcc)
 find_arm_compiler(CXX arm-none-eabi-g++)
+
+set(CMAKE_ASM_COMPILER_TARGET arm-none-eabi CACHE INTERNAL "")
+set(CMAKE_C_COMPILER_TARGET arm-none-eabi CACHE INTERNAL "")
+set(CMAKE_CXX_COMPILER_TARGET arm-none-eabi CACHE INTERNAL "")
+
+execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# TODO: Try locate sysroot, this should be used to find the arm-none-eabi/thumb libraries and gcc/thumb libraries
+get_filename_component(COMPILER_DIRECTORY "${CMAKE_ASM_COMPILER}" DIRECTORY)
+if(COMPILER_DIRECTORY MATCHES "/bin/?$")
+    get_filename_component(COMPILER_DIRECTORY "${COMPILER_DIRECTORY}" DIRECTORY)
+endif()
+set(CMAKE_FIND_ROOT_PATH "${COMPILER_DIRECTORY}/lib/arm-none-eabi" "${COMPILER_DIRECTORY}/lib/gcc/arm-none-eabi/${GCC_VERSION}" CACHE INTERNAL "")
+unset(COMPILER_DIRECTORY)
+
+if(USE_DEVKITARM)
+    set(CMAKE_EXE_LINKER_FLAGS_INIT "${SHARED_LINKER_FLAGS}" CACHE INTERNAL "" FORCE)
+else()
+    # Unfortunately ARM GNU toolchain compiles with short enums
+    # This causes a 32-bit enum warning to be emitted, even if all binaries use 32-bit enums
+    set(CMAKE_EXE_LINKER_FLAGS_INIT "${SHARED_LINKER_FLAGS} -Xlinker -no-enum-size-warning" CACHE INTERNAL "" FORCE)
+endif()
