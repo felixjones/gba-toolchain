3a1c61ab36a8b9248c5ce7608470b5480ab8e4f6
Felix Jones <felix@felixjones.co.uk>
2022-12-19 23:51:35 +0100

    Clang support GNU isystem headers

--- cmake/agb.toolchain.cmake
+++ cmake/agb.toolchain.cmake
@@ -15,16 +15,16 @@ if(CMAKE_GENERATOR STREQUAL "Unix Makefiles" AND NOT CMAKE_MAKE_PROGRAM)
 endif()
 
 # TODO: Set up linker to allow executable test compile
-set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY" CACHE INTERNAL "")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY CACHE INTERNAL "")
 
 function(find_arm_compiler lang binary)
     macro(detect_compiler_id)
-        get_filename_component(COMPILER_BASENAME "${CMAKE_${lang}_COMPILER}" NAME)
+        set(COMPILER_BASENAME "${CMAKE_${lang}_COMPILER}")
 
-        if(COMPILER_BASENAME MATCHES "(as)|(g(cc)|(\\+\\+))")
-            set(CMAKE_${lang}_COMPILER_ID GNU CACHE INTERNAL "")
-        elseif(COMPILER_BASENAME MATCHES "clang(\\+\\+)?")
+        if(COMPILER_BASENAME MATCHES "($|[^a-zA-Z])((clang)|(clang\\+\\+))")
             set(CMAKE_${lang}_COMPILER_ID Clang CACHE INTERNAL "")
+        elseif(COMPILER_BASENAME MATCHES "($|[^a-zA-Z])((as)|(gcc)|(g\\+\\+))")
+            set(CMAKE_${lang}_COMPILER_ID GNU CACHE INTERNAL "")
         else()
             message(FATAL_ERROR "Unknown compiler ${CMAKE_${lang}_COMPILER}")
         endif()
@@ -33,13 +33,13 @@ function(find_arm_compiler lang binary)
     if(CMAKE_${lang}_COMPILER)
         detect_compiler_id()
 
-        if(CMAKE_${lang}_COMPILER_ID MATCHES Clang)
+        if(CMAKE_${lang}_COMPILER_ID MATCHES "Clang")
             execute_process(COMMAND ${CMAKE_${lang}_COMPILER} -print-targets OUTPUT_VARIABLE TARGETS OUTPUT_STRIP_TRAILING_WHITESPACE)
 
             if(NOT TARGETS MATCHES "arm[ \t\r\n]*")
                 unset(CMAKE_${lang}_COMPILER CACHE)
             endif()
-        elseif(CMAKE_${lang}_COMPILER_ID MATCHES GNU)
+        elseif(CMAKE_${lang}_COMPILER_ID MATCHES "GNU")
             if(lang STREQUAL ASM)
                 execute_process(COMMAND ${CMAKE_${lang}_COMPILER} --version OUTPUT_VARIABLE DUMP OUTPUT_STRIP_TRAILING_WHITESPACE)
             else()
@@ -53,7 +53,7 @@ function(find_arm_compiler lang binary)
     endif()
 
     if(NOT CMAKE_${lang}_COMPILER)
-        find_program(CMAKE_${lang}_COMPILER NAMES ${binary} PATHS ENV DEVKITARM "$ENV{DEVKITPRO}/devkitARM" PATH_SUFFIXES "bin" REQUIRED)
+        find_program(CMAKE_${lang}_COMPILER NAMES ${binary} PATHS ENV DEVKITARM "$ENV{DEVKITPRO}/devkitARM" PATH_SUFFIXES bin REQUIRED)
     endif()
 
     detect_compiler_id()
@@ -63,24 +63,23 @@ find_arm_compiler(ASM arm-none-eabi-as)
 find_arm_compiler(C arm-none-eabi-gcc)
 find_arm_compiler(CXX arm-none-eabi-g++)
 
+find_program(SYSROOT_COMPILER NAMES arm-none-eabi-gcc PATHS ENV DEVKITARM "$ENV{DEVKITPRO}/devkitARM" PATH_SUFFIXES bin REQUIRED)
+execute_process(COMMAND "${SYSROOT_COMPILER}" -dumpversion OUTPUT_VARIABLE SYSROOT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# TODO: Try locate sysroot, this should be used to find the arm-none-eabi/thumb libraries and gcc/thumb libraries
+get_filename_component(SYSROOT_DIRECTORY "${SYSROOT_COMPILER}" DIRECTORY)
+unset(SYSROOT_COMPILER)
+if(SYSROOT_DIRECTORY MATCHES /bin/?$)
+    get_filename_component(SYSROOT_DIRECTORY "${SYSROOT_DIRECTORY}" DIRECTORY)
+endif()
+find_path(SYSROOT_DIRECTORY arm-none-eabi PATHS "${SYSROOT_DIRECTORY}" PATH_SUFFIXES lib REQUIRED)
+set(CMAKE_SYSROOT "${SYSROOT_DIRECTORY}/arm-none-eabi" CACHE INTERNAL "")
+unset(SYSROOT_DIRECTORY)
+
 set(CMAKE_ASM_COMPILER_TARGET arm-none-eabi CACHE INTERNAL "")
 set(CMAKE_C_COMPILER_TARGET arm-none-eabi CACHE INTERNAL "")
 set(CMAKE_CXX_COMPILER_TARGET arm-none-eabi CACHE INTERNAL "")
 
-execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-# TODO: Try locate sysroot, this should be used to find the arm-none-eabi/thumb libraries and gcc/thumb libraries
-get_filename_component(COMPILER_DIRECTORY "${CMAKE_ASM_COMPILER}" DIRECTORY)
-if(COMPILER_DIRECTORY MATCHES "/bin/?$")
-    get_filename_component(COMPILER_DIRECTORY "${COMPILER_DIRECTORY}" DIRECTORY)
-endif()
-set(CMAKE_FIND_ROOT_PATH "${COMPILER_DIRECTORY}/lib/arm-none-eabi" "${COMPILER_DIRECTORY}/lib/gcc/arm-none-eabi/${GCC_VERSION}" CACHE INTERNAL "")
-unset(COMPILER_DIRECTORY)
-
-if(USE_DEVKITARM)
-    set(CMAKE_EXE_LINKER_FLAGS_INIT "${SHARED_LINKER_FLAGS}" CACHE INTERNAL "" FORCE)
-else()
-    # Unfortunately ARM GNU toolchain compiles with short enums
-    # This causes a 32-bit enum warning to be emitted, even if all binaries use 32-bit enums
-    set(CMAKE_EXE_LINKER_FLAGS_INIT "${SHARED_LINKER_FLAGS} -Xlinker -no-enum-size-warning" CACHE INTERNAL "" FORCE)
-endif()
+set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES "${CMAKE_SYSROOT}/include" CACHE INTERNAL "")
+set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES "${CMAKE_SYSROOT}/include/c++/${SYSROOT_VERSION};${CMAKE_SYSROOT}/include/c++/${SYSROOT_VERSION}/arm-none-eabi/thumb" CACHE INTERNAL "") # TODO: Support using arm include for -marm sources
+unset(SYSROOT_VERSION)
