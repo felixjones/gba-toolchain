8e7f71a11f2c5c7ac2b54163915a0471f704aef4
felixjones <felix.jones@mojang.com>
2022-12-30 20:35:12 +0000

    Add objcopy install step

--- cmake/Platform/AdvancedGameBoy.cmake
+++ cmake/Platform/AdvancedGameBoy.cmake
@@ -1,2 +1,21 @@
 set(CMAKE_EXECUTABLE_FORMAT ELF CACHE INTERNAL "")
 set(CMAKE_EXECUTABLE_SUFFIX .elf CACHE INTERNAL "")
+
+function(install_rom target)
+    if(NOT TARGET ${target})
+        message(FATAL_ERROR "No target \"${target}\"")
+        return()
+    endif()
+
+    #TODO: Parse args for DESTINATION
+
+    unset(CMAKE_INSTALL_BINDIR)
+    install(TARGETS ${target} DESTINATION ".")
+    install(CODE "
+        execute_process(
+            COMMAND \"${CMAKE_OBJCOPY}\" -O binary \"$<TARGET_FILE_NAME:${target}>\" \"$<TARGET_FILE_BASE_NAME:${target}>.bin\"
+            WORKING_DIRECTORY \"${CMAKE_INSTALL_PREFIX}\"
+        )
+    ")
+    #TODO: gbafix
+endfunction()
