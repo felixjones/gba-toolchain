e1098cc6b61a6406754ae9be122f9fe06c6c991d
Felix Jones <felix@felixjones.co.uk>
2023-01-08 02:31:20 +0100

    Stub out all syscalls as ENOSYS

--- lib/rom/syscalls.c
+++ lib/rom/syscalls.c
@@ -9,40 +9,25 @@
 #undef errno
 extern int errno;
 
-int _close(int file) {
-    return -1;
-}
-
-int _fstat(int file, struct stat *st) {
-    st->st_mode = S_IFCHR;
-    return 0;
-}
-
-int _getpid(void) {
-    return 1;
-}
-
-int _isatty(int file) {
-    return 1;
-}
-
-int _kill(int pid, int sig) {
-    errno = EINVAL;
-    return -1;
-}
-
-int _lseek(int file, int ptr, int dir) {
-    return 0;
-}
-
-int _read(int file, char *ptr, int len) {
-    return 0;
-}
-
-int _write(int file, char *ptr, int len) {
-    /* TODO: Log to emulator console */
+static int stub() {
     errno = ENOSYS;
     return -1;
 }
+
+int _close(int file) __attribute__((alias("stub")));
+
+int _fstat(int file, struct stat *st) __attribute__((alias("stub")));
+
+int _getpid(void) __attribute__((alias("stub")));
+
+int _isatty(int file) __attribute__((alias("stub")));
+
+int _kill(int pid, int sig) __attribute__((alias("stub")));
+
+int _lseek(int file, int ptr, int dir) __attribute__((alias("stub")));
+
+int _read(int file, char *ptr, int len) __attribute__((alias("stub")));
+
+int _write(int file, char *ptr, int len) __attribute__((alias("stub")));
 
 #pragma clang diagnostic pop
