5794b1c878b8e0fca24f15980a1f6046ffce5c34
Felix Jones <felix@felixjones.co.uk>
2023-01-08 02:39:33 +0100

    Ignore syscalls.c alias warnings

--- lib/rom/syscalls.c
+++ lib/rom/syscalls.c
@@ -1,6 +1,3 @@
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "bugprone-reserved-identifier"
-
 /* Stubs based on https://sourceware.org/newlib/libc.html */
 
 #include <errno.h>
@@ -9,10 +6,16 @@
 #undef errno
 extern int errno;
 
-static int stub() {
+static int stub(void) {
     errno = ENOSYS;
     return -1;
 }
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "bugprone-reserved-identifier"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattribute-alias"
 
 int _close(int file) __attribute__((alias("stub")));
 
@@ -29,5 +32,7 @@ int _lseek(int file, int ptr, int dir) __attribute__((alias("stub")));
 int _read(int file, char *ptr, int len) __attribute__((alias("stub")));
 
 int _write(int file, char *ptr, int len) __attribute__((alias("stub")));
+
+#pragma GCC diagnostic pop
 
 #pragma clang diagnostic pop
