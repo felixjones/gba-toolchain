c956fbe95ee4fe68b66d6747068bdcbeb30f9835
Felix Jones <felix@felixjones.co.uk>
2023-02-27 14:07:48 +0100

    FindAgbabi.cmake pointed to new agbabi version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <tonc.h>
-
-#include <aeabi.h>
 #include <agbabi.h>
 
 [[gnu::section(".cart.backup"), gnu::used]]
@@ -8,27 +6,7 @@ constexpr const char save_type[] = "FLASH512_Vnnn";
 
 void render();
 
-alignas(int) unsigned char u8_array[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
-
 int main() {
-    __aeabi_memclr(u8_array + 3, 7);
-
-    int stack[40];
-    __agbabi_coro_t coro;
-
-    __agbabi_coro_make(&coro, stack + 40, [](auto* self) {
-        int count = 10;
-        while (--count) {
-            __agbabi_coro_yield(self, count);
-        }
-        return 0;
-    });
-
-    int val;
-    do {
-        val = __agbabi_coro_resume(&coro);
-    } while (val);
-
     REG_DISPCNT = DCNT_BG2 | DCNT_MODE3;
     render();
 }
