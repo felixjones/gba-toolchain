f89a76ca0467f7ae2413c9f9a60664acd40e455d
Felix Jones <felix@felixjones.co.uk>
2023-03-06 14:51:54 +0100

    install_rom CONCAT_ALIGN for aligning concatenated files to a byte boundary (eg: GBFS archives to 256 bytes)

--- cmake/Concat.cmake
+++ cmake/Concat.cmake
@@ -1,13 +1,21 @@
 include("${CMAKE_CURRENT_LIST_DIR}/IHex.cmake")
 
-function(binconcat first)
-    # TODO: Alignment
+function(binconcat align first)
+    macro(pad str)
+        if(${align} GREATER 1)
+            string(LENGTH ${${str}} slen)
+            math(EXPR slen "((${slen} / 2 + ${align} - 1) / ${align}) * ${align} - ${slen} / 2")
+            string(REPEAT "00" ${slen} padding)
+            string(APPEND ${str} ${padding})
+        endif()
+    endmacro()
 
     file(READ "${first}" temp HEX)
-    list(APPEND binary ${temp})
+    string(APPEND binary ${temp})
     foreach(arg ${ARGN})
+        pad(binary)
         file(READ "${arg}" temp HEX)
-        list(APPEND binary ${temp})
+        string(APPEND binary ${temp})
     endforeach()
 
     ihex(outputHex RECORD_LENGTH 0xff "${binary}")
