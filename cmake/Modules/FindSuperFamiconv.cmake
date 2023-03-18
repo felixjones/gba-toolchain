8983cf4e72956547db01202ca24b46eaacbcf3d8
Felix Jones <felix@felixjones.co.uk>
2023-03-18 17:39:44 +0100

    Basic superfamiconv workflow

--- cmake/Modules/FindSuperFamiconv.cmake
+++ cmake/Modules/FindSuperFamiconv.cmake
@@ -57,3 +57,95 @@ endif()
 if(NOT CMAKE_SUPERFAMICONV_PROGRAM)
     message(WARNING "superfamiconv not found: Please set `-DCMAKE_SUPERFAMICONV_PROGRAM:FILEPATH=<path/to/bin/superfamiconv>`")
 endif()
+
+set(SUPERFAMICONV_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../SuperFamiconv.cmake")
+
+function(add_superfamiconv_graphics target)
+    set(oneValueArgs
+        EXCLUDE_FROM_ALL
+        PALETTE
+        TILES
+        MAP
+    )
+
+    cmake_parse_arguments(ARGS "${oneValueArgs}" "" "" ${ARGN})
+
+    if(NOT ARGS_PALETTE OR NOT ARGS_TILES OR NOT ARGS_MAP)
+        message(FATAL_ERROR "add_superfamiconv_graphics requires PALETTE, TILES, or MAP")
+    endif()
+
+    set(SOURCES $<TARGET_PROPERTY:${target},SOURCES>)
+
+    if(NOT ARGS_EXCLUDE_FROM_ALL)
+        set(INCLUDE_WITH_ALL ALL)
+    endif()
+
+    # TODO: Find a bug reference for the below hack
+    string(REGEX REPLACE "([][+.*()^])" "\\\\\\1" SOURCES_BUG_FIX "${CMAKE_BINARY_DIR}/CMakeFiles/${target}")
+
+    file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/_stamp")
+    set(STAMP "${CMAKE_BINARY_DIR}/_stamp/${target}.stamp")
+
+    set(commands)
+    if(ARGS_PALETTE)
+        list(APPEND commands COMMAND "${CMAKE_COMMAND}" -DPALETTE=ON
+            "-DPROGRAM=${CMAKE_SUPERFAMICONV_PROGRAM}"
+            "-DPREFIX=$<TARGET_GENEX_EVAL:${target},$<TARGET_PROPERTY:${target},PREFIX_PALETTE>>"
+            "-DSUFFIX=$<TARGET_PROPERTY:${target},SUFFIX_PALETTE>"
+            "-DINPUTS=$<FILTER:${SOURCES},EXCLUDE,${SOURCES_BUG_FIX}|[.]rule>"
+            -P ${SUPERFAMICONV_SCRIPT}
+        )
+    endif()
+    if(ARGS_TILES)
+        list(APPEND commands COMMAND "${CMAKE_COMMAND}" -DTILES=ON
+            "-DPROGRAM=${CMAKE_SUPERFAMICONV_PROGRAM}"
+            "-DPREFIX=$<TARGET_GENEX_EVAL:${target},$<TARGET_PROPERTY:${target},PREFIX_TILES>>"
+            "-DSUFFIX=$<TARGET_PROPERTY:${target},SUFFIX_TILES>"
+            "-DINPUTS=$<FILTER:${SOURCES},EXCLUDE,${SOURCES_BUG_FIX}|[.]rule>"
+            "-DPREFIX_PALETTE=$<TARGET_GENEX_EVAL:${target},$<TARGET_PROPERTY:${target},PREFIX_PALETTE>>"
+            "-DSUFFIX_PALETTE=$<TARGET_PROPERTY:${target},SUFFIX_PALETTE>"
+            -P ${SUPERFAMICONV_SCRIPT}
+        )
+    endif()
+    if(ARGS_MAP)
+        list(APPEND commands COMMAND "${CMAKE_COMMAND}" -DMAP=ON
+            "-DPROGRAM=${CMAKE_SUPERFAMICONV_PROGRAM}"
+            "-DPREFIX=$<TARGET_GENEX_EVAL:${target},$<TARGET_PROPERTY:${target},PREFIX_MAP>>"
+            "-DSUFFIX=$<TARGET_PROPERTY:${target},SUFFIX_MAP>"
+            "-DINPUTS=$<FILTER:${SOURCES},EXCLUDE,${SOURCES_BUG_FIX}|[.]rule>"
+            "-DPREFIX_PALETTE=$<TARGET_GENEX_EVAL:${target},$<TARGET_PROPERTY:${target},PREFIX_PALETTE>>"
+            "-DSUFFIX_PALETTE=$<TARGET_PROPERTY:${target},SUFFIX_PALETTE>"
+            "-DPREFIX_TILES=$<TARGET_GENEX_EVAL:${target},$<TARGET_PROPERTY:${target},PREFIX_TILES>>"
+            "-DSUFFIX_TILES=$<TARGET_PROPERTY:${target},SUFFIX_TILES>"
+            -P ${SUPERFAMICONV_SCRIPT}
+        )
+    endif()
+
+    add_custom_command(OUTPUT ${STAMP}
+        ${commands}
+        COMMAND "${CMAKE_COMMAND}" -E touch ${STAMP}
+        DEPENDS $<FILTER:${SOURCES},EXCLUDE,${SOURCES_BUG_FIX}|[.]rule>
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        VERBATIM
+        COMMENT "Generating ${target}"
+    )
+
+    add_custom_target(${target} ${INCLUDE_WITH_ALL} DEPENDS ${STAMP})
+
+    set_target_properties(${target} PROPERTIES
+        OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}"
+        OUTPUT_NAME "${target}"
+        PREFIX_PALETTE "$<TARGET_PROPERTY:${target},OUTPUT_DIRECTORY>/"
+        PREFIX_TILES "$<TARGET_PROPERTY:${target},OUTPUT_DIRECTORY>/"
+        PREFIX_MAP "$<TARGET_PROPERTY:${target},OUTPUT_DIRECTORY>/"
+        SUFFIX_PALETTE ".palette"
+        SUFFIX_TILES ".tiles"
+        SUFFIX_MAP ".map"
+    )
+
+    if(ARGS_UNPARSED_ARGUMENTS)
+        set_target_properties(${target} PROPERTIES
+            SOURCES "${ARGS_UNPARSED_ARGUMENTS}"
+        )
+    endif()
+endfunction()
---
+++ cmake/SuperFamiconv.cmake
@@ -0,0 +1,38 @@
+if(PALETTE)
+    foreach(INPUT ${INPUTS})
+        get_filename_component(OUTPUT "${INPUT}" NAME_WE)
+        execute_process(COMMAND "${PROGRAM}" palette
+            --in-image "${INPUT}"
+            --out-data "${PREFIX}${OUTPUT}${SUFFIX}"
+            --mode gba
+        )
+    endforeach()
+endif()
+
+if(TILES)
+    foreach(INPUT ${INPUTS})
+        get_filename_component(OUTPUT "${INPUT}" NAME_WE)
+        set(INPUT_PALETTE "${PREFIX_PALETTE}${OUTPUT}${SUFFIX_PALETTE}")
+        execute_process(COMMAND "${PROGRAM}" tiles
+            --in-image "${INPUT}"
+            --in-palette "${INPUT_PALETTE}"
+            --out-data "${PREFIX}${OUTPUT}${SUFFIX}"
+            --mode gba
+        )
+    endforeach()
+endif()
+
+if(MAP)
+    foreach(INPUT ${INPUTS})
+        get_filename_component(OUTPUT "${INPUT}" NAME_WE)
+        set(INPUT_PALETTE "${PREFIX_PALETTE}${OUTPUT}${SUFFIX_PALETTE}")
+        set(INPUT_TILES "${PREFIX_TILES}${OUTPUT}${SUFFIX_TILES}")
+        execute_process(COMMAND "${PROGRAM}" map
+            --in-image "${INPUT}"
+            --in-palette "${INPUT_PALETTE}"
+            --in-tiles "${INPUT_TILES}"
+            --out-data "${PREFIX}${OUTPUT}${SUFFIX}"
+            --mode gba
+        )
+    endforeach()
+endif()
