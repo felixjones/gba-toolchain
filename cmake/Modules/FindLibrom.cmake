0f924ed9c4afc94f071aa259139a8c0187782c68
Felix Jones <felix@felixjones.co.uk>
2023-04-14 22:12:20 +0200

    Add gba-hpp

--- cmake/Modules/FindLibrom.cmake
+++ cmake/Modules/FindLibrom.cmake
@@ -1,1 +1,1 @@
-add_subdirectory("${CMAKE_SYSTEM_LIBRARY_PATH}/rom" "${CMAKE_BINARY_DIR}/lib/rom")
+add_subdirectory("${CMAKE_SYSTEM_LIBRARY_PATH}/rom" "${CMAKE_BINARY_DIR}/lib/rom" EXCLUDE_FROM_ALL)
