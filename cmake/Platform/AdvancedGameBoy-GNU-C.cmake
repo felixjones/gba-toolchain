ba547bda3876f1fbec77f5f9801ca2d8fbffab6a
Felix Jones <felix@felixjones.co.uk>
2023-07-16 20:22:09 +0200

    Default compile flags for C/C++

--- cmake/Platform/AdvancedGameBoy-GNU-C.cmake
+++ cmake/Platform/AdvancedGameBoy-GNU-C.cmake
@@ -1,3 +1,8 @@
 include(Compiler/CMakeCommonCompilerMacros)
 include(${CMAKE_ROOT}/Modules/CMakeDetermineCompileFeatures.cmake)
 cmake_determine_compile_features(C)
+
+set(CMAKE_C_FLAGS_RELEASE_INIT          "-O3 -DNDEBUG")
+set(CMAKE_C_FLAGS_DEBUG_INIT            "-O0 -g -D_DEBUG")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT   "-Og -g -DNDEBUG")
+set(CMAKE_C_FLAGS_MINSIZEREL_INIT       "-Os -DNDEBUG")
