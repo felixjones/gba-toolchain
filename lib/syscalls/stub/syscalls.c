3e7b1a643ff09b662073a51bb8908142284e1212
Felix Jones <felix@felixjones.co.uk>
2024-10-04 14:17:55 +0200

    More syscalls.c stubs

--- lib/syscalls/stub/syscalls.c
+++ lib/syscalls/stub/syscalls.c
@@ -10,6 +10,7 @@
 /* Stubs based on https://sourceware.org/newlib/libc.html */
 
 #include <errno.h>
+#include <sys/stat.h>
 
 static int stub(void) {
     errno = ENOSYS;
@@ -22,5 +23,17 @@ static int stub(void) {
 int _getpid(void) __attribute__((alias("stub")));
 
 int _kill(int pid, int sig) __attribute__((alias("stub")));
+
+int _close_r(struct _reent *ptr, int fd) __attribute__((alias("stub")));
+
+int _fstat_r(struct _reent *ptr, int fd, struct stat *pstat) __attribute__((alias("stub")));
+
+int _isatty_r(struct _reent *ptr, int fd) __attribute__((alias("stub")));
+
+off_t _lseek_r(struct _reent *ptr, int fd, off_t pos, int whence) __attribute__((alias("stub")));
+
+int _read_r(struct _reent *ptr, int fd, void* buf, size_t cnt) __attribute__((alias("stub")));
+
+int _write_r(struct _reent *ptr, int fd, const void* buf, size_t cnt) __attribute__((alias("stub")));
 
 #pragma GCC diagnostic pop
